9d497373a7423e0a6cae7513590f20e62e7c37c8
Jarvanmo <jarvan.mo@gmail.com>
2026-04-30 16:23:01 +0800

    initial commit

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version: 5.7
+import PackageDescription
+
+let package = Package(
+    name: "WechatOpenSDK",
+    platforms: [
+        .iOS(.v12)
+    ],
+    products: [
+        .library(
+            name: "WechatOpenSDK",
+            targets: ["WechatOpenSDK"]
+        ),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "WechatOpenSDK",
+            path: "WechatOpenSDK-NoPay.xcframework"
+        )
+    ]
+)
